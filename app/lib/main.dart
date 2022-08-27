c8e1d425e8fd41267b4ba934b1046d36a4071163
jsleemaster <roeowo123@naver.com>
2022-08-27 23:32:39 +0900

    StatefulWidget 설명

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class RandomWordsState extends State<RandomWords> {
+  // TODO Add build() method
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -14,7 +24,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
