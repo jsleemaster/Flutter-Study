59f80a8bcabf67608a9f95ae14a7e72ffa08fa0c
이순명 <75053960+jsleemaster@users.noreply.github.com>
2024-04-16 14:43:45 +0900

    Study pomodors dart

---
+++ app/lib/pomodors.dart
@@ -0,0 +1,111 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  int totalCount = 0;
+  late Timer timer;
+  bool isRunning = false;
+
+  void onTic(Timer timer) {
+    if (totalSeconds == 0) {
+      setState(() {
+        totalSeconds = 1500;
+        totalCount = totalCount + 1;
+        isRunning = false;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds = totalSeconds - 1;
+      });
+    }
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(const Duration(seconds: 1), onTic);
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onStopPressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  String format(int seconds) {
+    var time =
+        Duration(seconds: seconds).toString().split('.').first.substring(2);
+
+    return time;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Flexible(
+              flex: 1,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Text(format(totalSeconds)),
+              )),
+          Flexible(
+            flex: 2,
+            child: Center(
+              child: IconButton(
+                iconSize: 120,
+                color: Theme.of(context).cardColor,
+                onPressed: isRunning ? onStopPressed : onStartPressed,
+                icon: Icon(isRunning
+                    ? Icons.pause_circle_rounded
+                    : Icons.play_circle_outlined),
+              ),
+            ),
+          ),
+          Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).cardColor,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("$totalCount",
+                              style: TextStyle(
+                                fontSize: 28,
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .titleLarge
+                                    ?.color,
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
